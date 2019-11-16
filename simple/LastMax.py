9f7b23efa6f840048bb0becb689d44e0d2727438
quyixiao <2621048238@qq.com>
2019-11-16 11:25:11 +0800

    提交修改

---
+++ simple/LastMax.py
@@ -0,0 +1,9 @@
+m = int(input('input first number >>>'))
+while True:
+    c = input('Input a number >>>>')
+    if c :
+        n = int(c)
+        if(n > m ):
+            m = n
+        print('Max is ', m)
+
