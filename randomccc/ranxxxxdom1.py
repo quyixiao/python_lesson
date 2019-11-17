5de036cc9be1ce715e5d71ca067e4bfd81df32e9
quyixiao <2621048238@qq.com>
2019-11-17 22:43:38 +0800

    提交修改

---
+++ randomccc/ranxxxxdom1.py
@@ -0,0 +1,27 @@
+import random
+
+for i in range(0, 10):
+    print(random.randint(1, 6), end='\t')
+
+print()
+
+print("===============================")
+
+for i in range(0, 10):
+    print(random.randrange(1, 6, 2), end='\t')
+print()
+print("===============================")
+
+for i in range(0, 10):
+    print(random.choice([1, 3, 5, 7]), end='\t')
+
+print()
+print("===============================")
+list3 = [1, 3, 5, 7, [8, 9, 10]]
+random.shuffle(list3)  # 打乱列表的元素
+
+print(list3)
+
+print("===============================")
+
+
