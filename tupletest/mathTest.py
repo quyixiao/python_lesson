d340dd1b87c623420cb1ff58359c9b43c979936d
quyixiao <2621048238@qq.com>
2019-11-20 21:40:57 +0800

    提交修改

---
+++ tupletest/mathTest.py
@@ -0,0 +1,26 @@
+import datetime
+import math
+
+start = datetime.datetime.now();
+count = 1
+primenumber = []
+flag = False
+upper_limit = 100
+for x in range(3, upper_limit, 2):
+    up = math.ceil(math.sqrt(x))
+    for i in primenumber:
+        if x % i == 0:
+            flag = True
+            break
+        if i >= up:
+            flag = False
+            break
+    if not flag:
+        primenumber.append(x)
+        count += 1
+print(count)
+end = (datetime.datetime.now() - start).total_seconds()
+print(primenumber)
+
+
+
