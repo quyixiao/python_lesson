bd351a1e1f8fa069d4b0812cb581e2f5304b11ce
quyixiao <2621048238@qq.com>
2019-11-24 11:08:39 +0800

    提交修改

--- yanghuisanjiao/YanghuiSanjiao3.py
+++ yanghuisanjiao/YanghuiSanjiao3.py
@@ -4,7 +4,23 @@ for i in range(6):
 
 newline = [1] * 6
 
-newline = [1 for i in range(6)] #这个方法是优化以后的方法
-
+newline = [1 for i in range(6)]  # 这个方法是优化以后的方法
 
 a = print("b")
+
+print(type(a))
+
+list = []
+list.append(1)
+list.append(2)
+print(list)
+t = ()
+t = tuple(range(1, 2, 7))
+print(t)
+# 元组只是只读的，所以，增，删除，改，方法都没有的
+t = (2, 4, 5, 6, 7)
+print(t)
+t = (1,)
+print(t)
+t = (1,) * 5
+print(t)
