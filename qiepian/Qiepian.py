a87e020038206f0352156800cd940b238632f9b5
quyixiao <2621048238@qq.com>
2019-11-26 19:39:08 +0800

    提交修改

---
+++ qiepian/Qiepian.py
@@ -0,0 +1,27 @@
+print('www.magedu.com'[4:10])
+
+print('www.magedu.com'[:10])
+
+print('www.magedu.com'[:])
+
+print('www.magedu.com'[:-2])
+
+print('www.magedu.com'[4:-4])
+
+print('www.magedu.com'[4:50])
+
+print('www.magedu.com'[-40:10])
+
+print(bytearray(b'www.magedu.com')[-10:10])  #
+
+print(tuple(b'www.magedu.com')[-10:10])
+
+print(list('www.magedu.com')[4:10:2])
+
+print(list('www.magedu.com')[-4:-10:-2])
+
+print(list('www.magedu.com')[-10:-4:2])
+
+print(list('www.magedu.com')[-10:-20:-1])  # [start:stop:step]
+
+print(list('www.magedu.com')[::-1])  # [start:stop:step] 第m行的个数表示为
