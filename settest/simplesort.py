3123bbc579077407ab16f9febceef7c471ff2dd3
quyixiao <2621048238@qq.com>
2019-11-28 21:31:43 +0800

    提交修改

--- settest/simplesort.py
+++ settest/simplesort.py
@@ -1,3 +1,16 @@
 # 简单的排序
 # 属于选择排序
 #
+lst = [1, 9, 8, 5, 6, 7, 4, 3, 1, 2,3,4,5,2]
+length = len(lst)
+for i in range(length):
+    x = lst[i]
+    maxindex = i
+    for j in range(i + 1, length):
+        if lst[j] > lst[maxindex]:
+            maxindex = j
+    if i != maxindex:
+        lst[i], lst[maxindex] = lst[maxindex], lst[i]
+
+for i in lst:
+    print(i, end=' ')
