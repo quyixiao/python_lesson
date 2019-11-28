b0e07d4b656c2795cd0d361f10b68ef3e6a6142c
quyixiao <2621048238@qq.com>
2019-11-28 22:15:37 +0800

    提交修改

--- settest/simplesort1.py
+++ settest/simplesort1.py
@@ -1,17 +1,27 @@
 # 简单的排序
 # 属于选择排序
 #
-lst = [1, 9, 8, 5, 6, 7, 4, 3, 1, 2, 3, 4, 5, 2]
+lst = [1, 9, 8, 3, 2, 5, 4, 5, 2, 3, 1, 3, 4, 5, 6, 5, 2, 1]
 length = len(lst)
-for i in range(length // 2):
+
+for i in range(0, length // 2):
     maxindex = i
-    minindex = i
-    for j in range(i + 1, length):
-        if lst[j] > lst[maxindex]:
+    minindex = -i - 1
+    minorigin = minindex
+    for j in range(i + 1, length - i):
+        if lst[maxindex] < lst[j]:
             maxindex = j
-        if lst[j] < lst[minindex]:
-            minindex[j]
-    lst[i], lst[maxindex] = lst[maxindex], lst[i]
-    lst[2*i] ,lst[minindex] = lst[minindex] ,lst[2 * i ]
+        if lst[minindex] > lst[-j - 1]:
+            minindex = - j - 1
+    if i != maxindex:
+        lst[i], lst[maxindex] = lst[maxindex], lst[i]
+        if i == minindex or i == length + minindex:
+            minindex = maxindex
+
+    if minorigin != minindex:
+        tmp = lst[minorigin]
+        lst[minorigin] = lst[minindex]
+        lst[minindex] = tmp
+
 for i in lst:
     print(i, end=' ')
