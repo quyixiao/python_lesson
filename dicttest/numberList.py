3b15db1d9d6ca98bcc6f1245269d7229897ba160
quyixiao <2621048238@qq.com>
2019-11-30 15:47:36 +0800

    抢晒镜修改

--- dicttest/numberList.py
+++ dicttest/numberList.py
@@ -16,3 +16,24 @@ listkey = sorted(list(numDict.keys()))
 
 for k in listkey:
     print(k, ' ', numDict[k])
+
+strs = 'abcdefghijklmnopqrstuvwxyz'
+
+dict1 = {}
+for i in range(10000):
+    str1 = ''.join(random.sample(strs, 2))
+    if str1 not in dict1:
+        dict1[str1] = 1
+    else:
+        dict1[str1] += 1
+for j in reversed(sorted(dict1.keys())):
+    print(j, end=' ')
+    print('Repeat {} times '.format(dict1[j]))
+
+# 生成数据的过程和处理数据的情况怎样
+
+
+
+
+
+
