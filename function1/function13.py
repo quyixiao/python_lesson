702be75ab7a6ef32ffdbee3bb322740d66447cb1
quyixiao <2621048238@qq.com>
2019-12-04 21:10:44 +0800

    提交修改

--- function1/function13.py
+++ function1/function13.py
@@ -3,12 +3,13 @@ def foo(xyz=[], u='abc', z=123):
 
     def inner(a=10):
         pass
-    print(inner)       # 两个函数不一样 <function foo.<locals>.inner at 0x106556710>
+
+    print(inner)  # 两个函数不一样 <function foo.<locals>.inner at 0x106556710>
 
     def inner(a=100):
         print(xyz)
 
-    print(inner) # 两个函数不一样<function foo.<locals>.inner at 0x1064f68c0>
+    print(inner)  # 两个函数不一样<function foo.<locals>.inner at 0x1064f68c0>
     return inner
 
 
@@ -16,8 +17,7 @@ bar = foo()
 print(id(foo), id(bar), foo.__defaults__, bar.__defaults__)
 del bar
 print(id(foo), foo.__defaults__)
-print(id(foo), id(bar), foo.__defaults__, bar.__defaults__) #NameError: name 'bar' is not defined
+print(id(foo), id(bar), foo.__defaults__, bar.__defaults__)  # NameError: name 'bar' is not defined
 
-
-
-
+# 如果两个名称是一样的，那么就会覆盖，删除数据只是为了引用定义域，一样的，将其删除
+#
