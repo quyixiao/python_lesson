c27cf7d3e4c405b19be6b7600eafaee298d02150
quyixiao <2621048238@qq.com>
2019-12-10 21:20:15 +0800

    提交修改

--- inspect1/inspect3.py
+++ inspect1/inspect3.py
@@ -15,13 +15,13 @@ def check(fn):  # 检查传入的参数是否符合要求
         keys = list(params.keys())
         values = list(params.values())
         for i, val in enumerate(args):
-            if isinstance(val, values[i].annotation):
+            if values[i].annotation is not inspect._empty and isinstance(val, values[i].annotation):
                 print(keys[i], '==', val)
 
         # 关键字参数
         #
         for k, v in kwargs.items():
-            if isinstance(v, params[k].annotation):
+            if params[k].annotation is not inspect._empty and isinstance(v, params[k].annotation):
                 print(k, '=====', v)
         result = fn(*args, **kwargs)
         return result
@@ -36,3 +36,14 @@ def add(x: int, y: int = 7) -> int:
 
 print(add(4, y=3))
 
+print('--------------------------------')
+print(add(y=10, x=3))
+
+print('~~~~~~~~~~~~~~~~~~~~~~~~~~~~')
+# print(add(10,x = 4 )) #TypeError: add() got multiple values for argument 'x'
+# 业务应用
+# 有函数如下
+# 请检查用户输入的是否符合输入的要求
+# 最好让用户感觉不到你加入了东西，非侵入性的，在python 中在大量的装饰器的使用
+# 对用户输入的
+# 如果业务没有定义注解，
