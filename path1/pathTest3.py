33937a51d551b1117584e8346042c57aa090a198
quyixiao <2621048238@qq.com>
2020-01-03 14:17:44 +0800

    提交修改

--- path1/pathTest3.py
+++ path1/pathTest3.py
@@ -19,6 +19,51 @@ for x in p6.iterdir():
 
 p2 = Path()
 
+
 a = list(p2.glob("_*"))
 
 print(a)
+# 通配符
+# glob(pattern) 通配给定的模式
+# rglob(pattern) 通配给的模式，递归目录
+# 返回一个生成器
+# list(p.glob('test.*')) # 返回当前目录的对象下的test开头的文件
+# list(p.glob('**/*.py')) # 递归所有的目录，等同于rglob()
+# g = p.rglob('*.py') # 生成器
+# next(g)
+# 匹配 ，成功返回True
+#
+print(Path('a/b.py').match("*.py"))
+print(Path('a/b/c.py').match("b/*.py"))
+print(Path('a/b/c.py').match("a/*.py"))
+# stat() 相当于stat命令
+# lstat() 这个是看软链接的信息， 同 stat() ，但是如果符号相同 ，则这个东西转化成十进制
+# 这个拿过来之后，能不能用呢？如果想看软链接本身的话，则提供了文件的操作
+# p2.open() 使用方法类似内建函数open,返回一个文件的对象
+# 增加新的函数
+#
+# Path.read_text() # 这个表示以文本的模式打开，返回所有内容
+# Path.read_bytes() #  这个表示以字节的形式打开 ，这个表示 这个表示以字节的形式读取内容
+#
+# Path.write_bytes() 表示以写的形式来写数据，
+# 把你的数据给我，我直接写出去就可以了
+#
+
+p = Path('my_file')
+p.write_bytes(b'893298899832')
+print(p.read_bytes())
+p = Path('my_text_file')
+p.write_text('工9832983982893')
+p.read_text()
+
+
+
+
+
+
+
+
+
+
+
+
