301b903ebb82fb0b0124c791aab91d711184e8bd
quyixiao <2621048238@qq.com>
2020-01-14 13:45:01 +0800

    提交候 tgit

--- object1/Student.py
+++ object1/Student.py
@@ -15,7 +15,8 @@ class Student:
         return self.__name
 
 
-    def __del__(self):
+    def __del__(self): # 将自己相关的东西
+
         print('del')
 
 
@@ -26,5 +27,10 @@ jerry = Student('jerry')
 del tom
 print('------------------')
 time.sleep(3)
+del jerry
 print('=============end======================')
+#  实现由于 Python 实现回收机制，不能确定对象
+#  内存这个东西不能乱用的
+#  数据库连接，清理资源，如果注意资源的清理操作，如果要手动调用的话。
+#
 
