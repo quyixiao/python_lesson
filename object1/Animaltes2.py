90f983149931470c5623d2feffd3d92fb88e2e47
quyixiao <2621048238@qq.com>
2020-01-15 13:37:28 +0800

    提交修改

--- object1/Animaltes2.py
+++ object1/Animaltes2.py
@@ -4,24 +4,45 @@ class Animal:
     def __init__(self):
         self.y = 456
 
+    def shout1(self):
+        print('----------1111111111111111----------')
+
     def shout(self):
-        print('Animal shuts ')
+        print('Animal shuts ' , self.shout1())
+
+
+    def speek(self):
+        print('Animal speek ',self.__class__.__name__ ,self.y)
+
+
 
 class Cat(Animal):
     x = 'abc'
     def __init__(self):
         self.y = 'xyz'
 
-    def shout(self):
-        print('Maio')
 
-a = Animal()
-a.shout()
+    def shout1(self):
+        print('----------2222222222222222----------')
+    def shout(self):
+        print('--------------------')
+        Animal.shout(self)
+
+
+    def speek(self):
+        print('Cat speek ' )
+        super().speek()
+
+
+
+#a = Animal()
+#a.shout()
 
 c = Cat()
 c.shout()
+c.speek()
 print(c.x)
 print(c.y)
-print(a.y)
+#print(a.y)
 
 
