7ae7b8ec7499f2b4ca58537df021058a1ed11f9e
quyixiao <2621048238@qq.com>
2020-01-15 11:42:10 +0800

    提交修改

--- object1/Animal.py
+++ object1/Animal.py
@@ -21,8 +21,6 @@ dog = Dog()
 dog.shout()
 
 
-print(1,int.mro())
-print(2,int.__mro__)
-print(3,int.__base__)
-print(4,int.__bases__)
-print(5,int.__subclasses__())
+
+print(Dog.mro())
+print(Dog.__mro__)
---
+++ object1/IntTest.py
@@ -0,0 +1,6 @@
+
+print(1,int.mro())
+print(2,int.__mro__)
+print(3,int.__base__)
+print(4,int.__bases__)
+print(5,int.__subclasses__())
