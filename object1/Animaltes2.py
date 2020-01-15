7fab47d9a6820144c915ff44c9702642c2e54774
quyixiao <2621048238@qq.com>
2020-01-15 12:17:44 +0800

    提交修改

--- object1/Animaltes2.py
+++ object1/Animaltes2.py
@@ -1,9 +1,19 @@
 class Animal:
+    x = 123
+
+    def __init__(self):
+        self.y = 456
+
     def shout(self):
         print('Animal shuts ')
 
 
 class Cat(Animal):
+    x = 'abc'
+
+    def __init__(self):
+        self.y = 'xyz'
+
     def shout(self):
         print('Maio')
 
@@ -11,6 +21,8 @@ class Cat(Animal):
 a = Animal()
 a.shout()
 
-
 c = Cat()
 c.shout()
+print(c.x)
+print(c.y)
+print(a.y)
--- object1/printlat.py
+++ object1/printlat.py
@@ -14,7 +14,8 @@ class Point:
 ri = RandomGenerator1()
 points = [Point(x, y) for x, y in zip(ri.generator(), ri.generator())]
 points = [Point(*v) for v in zip(ri.generator(), ri.generator())]
-# print(list(points)) #  如果觉得惰性求值的方式是怎样的
+# print(list(points)) #  如果觉得惰性求值的方式是怎样的 python 是通过自己的字典来实现的
+
 for point in points:
     print(point)
 
