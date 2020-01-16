a02b7109f29fd5b73e3a6cf9e30983790825790c
quyixiao <2621048238@qq.com>
2020-01-16 23:10:25 +0800

    提交修改

--- object1/Cart.py
+++ object1/Cart.py
@@ -1,4 +1,6 @@
 # 将购物车改告成，
+# python 运算符的重载比 java 要牛逼得多
+
 class Cart:
     def __init__(self):
         self.items = []
@@ -13,10 +15,11 @@ class Cart:
 
     def additem(self,item):
         self.items.append(item)
-
+        return self
 
     def __add__(self, other):
         self.items.append(other)
+        return self
 
     def __getitem__(self, index):
         return self.items[index]
@@ -24,13 +27,15 @@ class Cart:
     def __setitem__(self, index, value):
         self.items.insert(index,value)
 
+    def __str__(self):
+        return "{}".format(self.items)
 
 cart = Cart()
-
 print(len(cart))
 cart.additem(1)
 cart.additem(2)
 cart.additem(3)
+cart.additem('abc')
 print(len(cart))
 print('--------------------------')
 for x in cart:
@@ -40,3 +45,10 @@ print(1 in cart)
 print(cart[2])
 
 print('+++++++++++++++++++++++++++++++++')
+print(cart)
+
+cart + 100 + 200 + 300 + 400
+print(cart)
+cart.additem(900).additem(300).additem(200)
+print(cart)
+
