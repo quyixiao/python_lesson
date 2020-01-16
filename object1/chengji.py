bfc1cd795e717863ddc0c996ddbf52603deeb78c
quyixiao <2621048238@qq.com>
2020-01-16 12:09:06 +0800

    提交修改

--- object1/chengji.py
+++ object1/chengji.py
@@ -1,11 +1,16 @@
 import bisect
 
-breakpoint = [60,70,80,90]
+breakpoint = [60, 70, 80, 90]
 grades = "EDCBA"
-
 score = 55
+a = bisect.bisect(breakpoint, score)
+print(a, grades[a])
 
-a = bisect.bisect(breakpoint,score)
+def get_grade(score, breakpoint=[60, 70, 80, 90], grades="EDCBA"):
+    return grades[bisect.bisect(breakpoint, score)]
 
 
-print(a,grades[a])
+get_grade1 = lambda score, breakpoint=[60, 70, 80, 90], grades="EDCBA": grades[bisect.bisect(breakpoint, score)]
+
+print(get_grade(55))
+print(get_grade1(55))
