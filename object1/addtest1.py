f0f32299e0a747698e0f90ed0f0934acf3ac9db2
quyixiao <2621048238@qq.com>
2020-01-17 15:01:15 +0800

    提交修改

--- object1/addtest1.py
+++ object1/addtest1.py
@@ -3,6 +3,17 @@ import datetime
 from functools import wraps, update_wrapper
 
 # 每个地方不同的类型 这个类可以当成一个函数使用，那么，我们就可以将他当成一个函数
+# 怎样写代码，是不是要初始化函数来实现，文档字符串来实现
+# 上下文应用场景
+# 增强功能
+# 在代码执行前后增加代码，以增强其功能，类似装饰器的功能
+# 资源管理
+# 打开了资源需要关闭，例如文件对象，网络连接，数据库连接等
+# 权限管理
+# 在执行代码之前，做权限的验证，有 __enter__中处理
+# 退出之前一定能做到资源清理
+# 锁，都用上下文管理
+#
 class TimeIt:
     """Time It class """
     def __init__(self, fn, output=lambda fn, delta: print("timeit : {} took {} s ".format(fn.__name__, delta))):
