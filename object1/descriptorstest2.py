75758e8dbef963927b2af8677a97d3f110715f98
quyixiao <2621048238@qq.com>
2020-01-19 14:32:36 +0800

    抢晒镜人瞩

--- object1/descriptorstest2.py
+++ object1/descriptorstest2.py
@@ -1,17 +1,21 @@
 # 描述器 Descripttors
-# 用到了3个魔术方法: __get__(),__set__()
+# 用到了3个魔术方法: __get__(),__set__(),__delete_ 三个方法中的任何一个，
+#  数据只有 get 的话，就是数据器
+#  非数据描述符，non-data-descriptor
+# 同时出现了__get__，
 
 class A:
     def __init__(self,x):
         self.x = x
         print('A__init__')
     def __set__(self, instance, value):
-        print(3,self,instance,value)
+        print(3,'A__set__',self,instance,value)
         return instance
 
 
     def __get__(self, instance, owner):
-        print(4,self,instance,owner)
+        print(4,'A__get__',self,instance,owner)
+        return instance
 
 
 class B:
@@ -20,14 +24,14 @@ class B:
         print('B___init__')
         self.a1 = 'b1'
 
-
-
 print(5,B.x)
 b = B()
 print(6,b.x)
 print(7,b.a1 )
+print(b.__dict__)
+print('-------------------------------')
+b.x= 100
+print(b.x)
 
-
-
-
+print(b.__dict__)
 
