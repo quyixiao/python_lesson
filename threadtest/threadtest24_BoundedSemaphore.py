aa5bffa4c11e9a13b1c46c4005c776cf4c4047bb
quyixiao <2621048238@qq.com>
2020-02-01 21:51:45 +0800

    提交修改

--- threadtest/threadtest24_BoundedSemaphore.py
+++ threadtest/threadtest24_BoundedSemaphore.py
@@ -9,7 +9,11 @@ from threading import BoundedSemaphore
 # semaphore信号量
 # 对于类似资源的使用的情况，我们一般用来做
 # 代价非常的高，
-#
+# 上例子中，BoundSemaphore类，有界的信号量，不允许使用release超出初始值的范围，否则，抛出ValueError异常
+# 将上例中信号量改成有界的信号量试一试
+# 应用举例
+# 连接池
+# 因为资源有限，且开启一个连接成本高，所以，使用连接池
 #
 FORMAT = '%(asctime)-15s \t [%(threadName)s ,%(thread)8d] %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
