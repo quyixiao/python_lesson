39938419137f687434e67f56005fd62d2adc1021
quyixiao <2621048238@qq.com>
2020-02-07 09:40:30 +0800

    提交修改

--- sockettest/sockettest7_udp_service.py
+++ sockettest/sockettest7_udp_service.py
@@ -6,7 +6,7 @@ import logging
 #
 FORMAT = '%(asctime)-15s \t [%(threadName)s ,%(thread)8d] %(message)s'
 logging.basicConfig(level=logging.INFO, format=FORMAT)
-
+# 我们经常使用心跳机制，先有一个基本的
 
 class ChatUdpServer:
     def __init__(self, ip='0.0.0.0', port=9999):
@@ -17,18 +17,26 @@ class ChatUdpServer:
 
     def start(self):
         self.socket.bind(self.addr)  # 启动绑定本地地址和端口udp
-        threading.Thread(target=self.recv, name='recv').start()
+        threading.Thread(target=self.recv, name='recv').start() #
 
     def recv(self):
         while not self.event.is_set():
             data, raddr = self.socket.recvfrom(1024)
             logging.info(data)
             logging.info(raddr)
+            if data.strip() == b'quit':
+                if raddr in self.clients:
+                    self.clients.remove(raddr)
+                continue
+
+            self.clients.add(raddr)
             msg = 'Ack {} .from :{}'.format(data, *raddr).encode()
             for c in self.clients:
                 self.socket.sendto(msg, c)
 
     def stop(self):
+        for c in self.clients:
+            self.socket.sendto(b'bye',c)
         self.socket.close()
         self.event.set()
 
---
+++ sockettest/sockettest8_udp_client.py
@@ -0,0 +1,57 @@
+import datetime
+import socket
+import threading
+import logging
+
+# 上面的例子中，如果客户端断开了，服务端知道了，每一个服务端还需要对所有的客户端发送的数据，包括已经断开的客户端
+# 代码改进
+# 加一个ack机制和心跳heartbeat，心跳，就是一端定时发往另一端的信息，一般每次数据越少越好，心跳时间间隔写好就好了
+# ack即响应，一端收到另一端的消息后返回的信息
+#
+FORMAT = '%(asctime)s 【%(levelname)s】 [%(filename)s:%(lineno)d] %(message)s'
+logging.basicConfig(level=logging.INFO, format=FORMAT)
+
+# client = socket.socket(type=socket.SOCK_DGRAM)
+# raddr = ('127.0.0.1', 9999)
+# client.connect(raddr)
+# client.sendto(b'hello', raddr)
+# data, addr = client.recvfrom(1024)
+# logging.info(data)
+# client.close()
+
+
+class ChatUdpClient:
+    def __init__(self,rip,rport=9999):
+        self.raddr = (rip,rport)
+        self.socket = socket.socket(type,socket.SOCK_DGRAM)
+        self.event = threading.Event()
+
+    def start(self):
+        self.socket.connect(self.raddr)
+        threading.Thread(target=self.recv,name='recv').start()
+
+    def recv(self):
+        while not self.event.is_set():
+            data,addr = self.socket.recvfrom(1024)
+            logging.info(data)
+            logging.info(addr)
+
+    def send(self,msg:str):
+        self.socket.sendto("{}\n".format(msg).encode())
+
+    def stop(self):
+        self.socket.close()
+
+def main():
+    cc = ChatUdpClient()
+    cc.start()
+    while True:
+        cmd  = input('Plz input you msg:')
+        if cmd.strip() == 'quit':
+            cc.stop()
+            break
+        logging.info(threading.enumerate())
+        cc.send(cmd)
+
+if __name__ == '__main__':
+    main()
