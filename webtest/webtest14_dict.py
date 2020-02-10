e77a5b5030c93e404e05c6987c73420465045447
quyixiao <2621048238@qq.com>
2020-02-11 00:17:38 +0800

    提交修改

--- webtest/webtest14_dict.py
+++ webtest/webtest14_dict.py
@@ -1,6 +1,9 @@
 #  字典属性化
 # dict 有没有效
 #
+import re
+
+
 class AttrDict:
     def __init__(self, d: dict):
         self.__dict__.update(d)
@@ -14,6 +17,7 @@ class AttrDict:
     def __len__(self):
         return len(self.__dict__)
 
+
 d = {'a': 100, 'b': 200}
 obj = AttrDict(d)
 print(obj.a)
@@ -21,3 +25,69 @@ print(obj.b)
 
 obj.__dict__['a'] = 2000
 print(obj.a)
+
+# str  不包含/的任意字符，默认类型               [^/]+
+# word 字母和数字                                \w+
+# int   纯粹数字，正负数                        [+-]?\d+
+# float     正负号，数字，包含               [+-]?\d+.\d+
+# any       包含/的任意字符            .+
+#  类型设计，支持 str,word ,int,float,any 类型
+# 还可以考虑一种 raw类型，
+# 问题，
+# 目前路由匹配使用正则表达式定义，不友好，很多的用户不会以使用正则表达式，能否简化
+# 分析
+# 生产环境，URL是规范，不能随便书写，路径是很有意义的，尤其是对 restful 风格
+# 所以，要对 URL规范
+#  例如/product/1110892398，这就是一种规范，也是规范了，背后的故事是什么
+#
+
+
+
+# '/{id:int} #=> '^/(?P<id>\d+)$'
+TYPEPATTERNS = {
+    'str': r'[^/]+',
+    'word': r'\w+',
+    'int': r'[+-]?\d+',
+    'float': r'[+-]?\d+\.\d+',
+    'any': r'.+'
+}
+name = 'id'
+t = 'int'
+print('/(?P<{}>){}'.format(name,TYPEPATTERNS.get(t,TYPEPATTERNS['str'])))
+
+print('*'*100)
+src = '/student/{name}/{id:int}/{age:int}'
+
+#pattern = r'/{([^{}:]*):([^{}:]*)}'
+pattern = r'/{([^{}:]+):?([^{}:]*)}'
+regex = re.compile(pattern)
+matcher = regex.search(src)
+print(matcher)
+print(matcher.groups())
+def repl(matcher):
+    name = matcher.group(1)
+    t = matcher.group(2)
+    return '/(?P<{}>){}'.format(name,TYPEPATTERNS.get(t,TYPEPATTERNS['str']))
+
+
+#print(regex.sub(repl,src))
+print('-----------')
+
+def parse(src:str):
+    start = 0
+    res = ''
+    for matcher in regex.finditer(src):
+        print(matcher)
+        res += src[start:matcher.start()]
+        name = matcher.group(1)
+        t = matcher.group(2)
+        tmp = '/(?P<{}>){}'.format(name, TYPEPATTERNS.get(t, TYPEPATTERNS['str']))
+        res += tmp
+        start = matcher.end()
+    else:
+        res += src[start:]
+    return res
+
+print(parse(src))
+
+
---
+++ webtest/webtest15_route.py
@@ -0,0 +1,165 @@
+import re
+from wsgiref.util import setup_testing_defaults
+from wsgiref.simple_server import make_server, demo_app
+from urllib.parse import parse_qs
+from webob import Request, Response
+
+from webob.dec import wsgify
+
+
+class AttrDict:
+    def __init__(self, d: dict):
+        self.__dict__.update(d)
+
+    def __setattr__(self, key, value):
+        raise NotImplementedError()
+
+    def __repr__(self):
+        return "{}".format(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+
+class Router:
+    # '/{id:int} #=> '^/(?P<id>\d+)$'
+    TYPEPATTERNS = {
+        'str': r'[^/]+',
+        'word': r'\w+',
+        'int': r'[+-]?\d+',
+        'float': r'[+-]?\d+\.\d+',
+        'any': r'.+'
+    }
+
+    TYPECAST = {
+        'str': str,
+        'word': str,
+        'int': int,
+        'float': float,
+        'any': str
+    }
+
+    _regex = re.compile(r'/{([^{}:]+):?([^{}:]*)}')
+
+    def __init__(self, prefix: str = ''):
+        self.__prefix = prefix.rstrip('/\\')  # 前缀，例如/product
+        self.__routetable = []  # 存在三元组，装饰器
+
+    def _repl(self, matcher):
+        name = matcher.group(1)
+        t = matcher.group(2)
+        return '/(?P<{}>){}'.format(name, self.TYPEPATTERNS.get(t, self.TYPEPATTERNS['str']))
+
+    def parse_back(self, rule):
+        return self._regex.sub(self._repl, rule)
+
+    def parse(self, src: str):
+        start = 0
+        res = ''
+        types = {}  # 字典
+        for matcher in self.__regex.finditer(src):
+            print(matcher)
+            res += src[start:matcher.start()]
+            name = matcher.group(1)
+            t = matcher.group(2)
+            types[name] = self.TYPECAST.get(t, str)  # 获取数据类型
+            tmp = '/(?P<{}>){}'.format(name, self.TYPEPATTERNS.get(t, self.TYPEPATTERNS['str']))
+            res += tmp
+            start = matcher.end()
+        else:
+            res += src[start:]
+        return res, types
+
+    def route(self, rule, *methods):
+        def wrapper(handler):
+            pattern, types = self.parse(rule)
+            self.__routetable.append(
+                (tuple(map(lambda x: x.upper(), methods)),
+                 re.compile(pattern), types, handler)
+            )
+            return handler
+
+        return wrapper
+
+    @classmethod
+    def get(cls, pattern):
+        return cls.route(pattern, 'GET')
+
+    @classmethod
+    def POST(cls, pattern):
+        return cls.route(pattern, 'POST')
+
+    @classmethod
+    def head(cls, pattern):
+        return cls.route(pattern, 'HEAD')
+
+    def match(self, request: Request):
+        # 必须先匹配前缀
+        if not request.path.startswith(self.__prefix):
+            return None
+        # 前缀匹配，说明就必须这个 Router 实例独步一时，后续匹配不上，依然返回 None
+        for methods, pattern, types, handler in self.__routetable:
+            # not methods 表示一个方法都没有定义，就是支持全部方法
+            if not methods or request.method.upper() in methods:
+                # 前提已经是以__prefix开头了，可以 replace，去掉 prefix 剩下的才是正则表达式需要匹配的路径
+                matcher = pattern.match(request.path.replace(self.__prefix, '', 1))
+                if matcher:  # 正则匹配
+                    # 动态为 request 增加属性
+                    request.groups = matcher.groups()  # 所有的分组组成的元组，包括命名分组
+                    # 解决类型问题
+                    newdict = {}
+                    for k,v in matcher.groupdict().items():
+                        newdict[k] = types[k](v)
+
+                    request.groupdict = AttrDict(newdict)  # 命名分组组成的字典
+                    return handler(request)
+
+
+class App:
+    _ROUTERS = []  # 存储所有的一级 Router 对象
+
+    # 注册
+    @classmethod
+    def register(cls, *routers: Router):
+        for router in routers:
+            cls._ROUTERS.append(router)
+
+    @wsgify
+    def __call__(self, request: Request):
+        # 遍历_ROUTERS,调用Router 实例的 match 方法，看谁匹配，
+        for router in self._ROUTERS:
+            response = router.match(request)
+            if response:  # 匹配返回的非 None 的 Router 对象
+                return response  # 匹配则立即返回
+        raise Exception('404')
+
+
+# 创建 Router 对象
+idx = Router()
+py = Router('/python')
+# 注册
+App.register(idx, py)
+
+
+@idx.get(r'^/$')
+def indexhandler(request: Request):
+    print(request.groups)
+    print(request.groupdict)
+    return 'indexhandler'
+
+
+@idx.route(r'/{id:int}')  # 支持所有的方法访问 ,'/{id:int} #=> '^/(?P<id>\d+)$'
+def pythonhandler(request: Request):
+    res = Response()
+    res.charset = 'utf-8'
+    res.body = 'welcom to ma python'.encode()
+    return res
+
+
+if __name__ == '__main__':
+    httpd = make_server('0.0.0.0', 8000, App())
+    print('SErver on port 8000...')
+    try:
+        httpd.serve_forever()
+    except:
+        httpd.server_close()
