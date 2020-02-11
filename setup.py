b667a7a4b4f26a36d8336bf8e7591db0cd20d5d8
quyixiao <2621048238@qq.com>
2020-02-11 20:25:35 +0800

    提交修改

--- setup.py
+++ setup.py
@@ -1,10 +1,23 @@
 from distutils.core import setup
+import glob
 
-setup(name='m',
-      version='1.0.1',
-      description='oweiieow',
-      author='quyixiao',
+#
+# setup(name='m',
+#       version='1.0.1',
+#       description='oweiieow',
+#       author='quyixiao',
+#       author_email='2621048238@qq.com',
+#       url='http://www.baidu.com',
+#       packages=['argparse1'],
+#       )
+
+
+setup(name='m', # name 名字
+      version='1.0.1', # version 版本信息
+      description='oweiieow', # 描述信息
+      author='quyixiao', #
       author_email='2621048238@qq.com',
       url='http://www.baidu.com',
-      packages=['argparse1'],
+      packages=['webarch'],  # 将非目录子包加入进来
+      data_file=glob.glob('webarch/templates/*.html')  # 加入模版
       )
