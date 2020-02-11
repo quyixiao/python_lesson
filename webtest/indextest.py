af8d6777d34e0ae988ce0175793664b85539072d
quyixiao <2621048238@qq.com>
2020-02-11 13:51:09 +0800

    拼接字符串

--- webtest/indextest.py
+++ webtest/indextest.py
@@ -1,17 +1,41 @@
 import re
+from io import StringIO
 
 pattern = r'{{([^{}]+)}}'
 regex = re.compile(pattern)
 
-def repl(matcher):
-    return str(d.get(matcher.group(1),''))
 d = {
     'id': 50,
     'name': 'tom',
     'age': 20
 }
 
+
+def repl(matcher):
+    return str(d.get(matcher.group(1), ''))
+
+
+# <p>{{id}} {{name}} {{age}}</p>
+def parse(line: str, data: dict):
+    start = 0
+    res = ''
+    for matcher in regex.finditer(line):
+        print(matcher)
+        res += line[start:matcher.start()]
+        res += str(data.get(matcher.group(1), ''))
+        start = matcher.end()
+    else:
+        res += line[start:]
+    return res
+
+
+out = StringIO()
+
 with open('index.html', encoding='utf-8') as f:
     for line in f:
-        ret = regex.sub(repl, line)
-        print(ret)
+        # ret = regex.sub(repl, line)
+        ret = parse(line, d)
+        out.write(ret)
+
+print('------------------------------')
+print(out.getvalue())
